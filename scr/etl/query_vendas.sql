4164c406ad8dc4b1813514de2b4267fc7f8f68cb
Matheus Morandi <theu.morandi@gmail.com>
2024-11-06 11:32:55 -0300

    Investigando tabelas de produtos

--- scr/etl/query_vendas.sql
+++ scr/etl/query_vendas.sql
@@ -1,16 +1,19 @@
 SELECT t2.seller_id,
     SUM ( t2.price ) AS receita_total,
-    COUNT ( DISTINCT t2.order_id ) AS qntd_vendas,
     SUM ( t2.price ) / COUNT ( DISTINCT t2.order_id ) AS avg_val_vendas,
+    SUM ( t2.price ) / 6 AS avg_val_vendas_mes,
+    COUNT ( DISTINCT t2.order_id ) AS qntd_vendas,
 
-    COUNT ( t2.product_id ) as qntd_produto,
-    COUNT ( DISTINCT t2.product_id ) as qntd_produto_dist
+    COUNT ( t2.product_id ) AS qntd_produto,
+    COUNT ( DISTINCT t2.product_id ) AS qntd_produto_dist,
+    SUM ( t2.price ) / COUNT ( t2.product_id ) AS avg_val_produto,
+    COUNT ( t2.product_id ) / COUNT ( DISTINCT t2.order_id ) AS avg_qntd_produto_venda
+
 
 FROM tb_orders AS t1
 
 LEFT JOIN tb_order_items AS t2
 ON t1.order_id = t2.order_id
-
 
 WHERE t1.order_approved_at BETWEEN '2016-10-01' 
 AND '2017-04-01'
